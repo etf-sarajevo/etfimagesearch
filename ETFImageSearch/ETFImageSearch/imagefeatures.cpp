7ecf38ecb752ed91da7b07a4ed5f814b34ae48bd
vljubovic <vljubovic@smartnet.ba>
2014-03-08 11:46:59 +0100

    Forgot to add ImageFeatures in commit before last

--- ETFImageSearch/ETFImageSearch/imagefeatures.cpp
+++ ETFImageSearch/ETFImageSearch/imagefeatures.cpp
@@ -1,9 +1,41 @@
-#include "searchalgorithm.h"
+#include "imagefeatures.h"
+#include "luetal_v2.h"
 
-SearchAlgorithm::SearchAlgorithm()
+#include <QDebug>
+
+ImageFeatures::ImageFeatures()
 {
 }
 
-DCTSearchAlgorithm::DCTSearchAlgorithm()
+DCTFeatures::DCTFeatures()
 {
+}
+
+ImageFeatures* ImageFeatures::factory(QString name)
+{
+	if (name == ColorHistogram::static_name())
+		return new ColorHistogram();
+	if (name == LuEtAl_v2::static_name())
+		return new LuEtAl_v2();
+}
+
+void ImageFeatures::vectorDump(QVector<double> vector) {
+	QString output;
+	for (uint i(0); i<vector.size(); i++)
+		output += QString("%1,").arg(vector[i]);
+	qDebug() << output;
+}
+
+void ImageFeatures::vectorDump(std::vector<double> vector) {
+	QString output;
+	for (uint i(0); i<vector.size(); i++)
+		output += QString("%1,").arg(vector[i]);
+	qDebug() << output;
+}
+
+void ImageFeatures::vectorDump(std::vector<int> vector) {
+	QString output;
+	for (uint i(0); i<vector.size(); i++)
+		output += QString("%1,").arg(vector[i]);
+	qDebug() << output;
 }
--- ETFImageSearch/ETFImageSearch/imagefeatures.h
+++ ETFImageSearch/ETFImageSearch/imagefeatures.h
@@ -1,65 +1,156 @@
-#ifndef SEARCHALGORITHM_H
-#define SEARCHALGORITHM_H
+#ifndef IMAGEFEATURES_H
+#define IMAGEFEATURES_H
 
 #include <QVector>
 #include <QString>
+#include <QMap>
+#include <vector>
 
-// Libraries from the hacked version of libjpeg
-extern "C" {
-//	#include "jpeglib.h"
-}
+#include "featurevector.h"
+
+#define REGISTER_CLASS
 
 
+/**
+ * Base class for algorithms for extracting features from image
+ */
 
-class FeatureVector {
-public:
-	// Here further metadata can be added
-	QVector<double> features;
-};
-
-class SearchAlgorithm
+class ImageFeatures
 {
 public:
-	SearchAlgorithm();
+	ImageFeatures();
 	
-	virtual QString name()=0; // should be a short string of lowercase letters and numerals (used in filename)
+	/**
+	 * Name of this feature extraction method (used as ID in index files, should be unique)
+	 */
+	virtual QString name()=0;
 	
-	// Get feature vector from an array containing image data
-	virtual FeatureVector extractFeatures(const uchar* imageData, int size)=0;
+	/**
+	 * Number of elements in feature vector 
+	 */
+	virtual int size()=0;
 	
+	enum DataType {
+		UINT8, INT8, UINT16, INT16, UINT32, INT32, FLOAT32, DOUBLE64
+	};
+	
+	/**
+	 * Data type for elements of feature vector. Used by indexer to optimize memory usage (internally feature vector is always double).
+	 * There is no point to use values with greater granularity because of memory alignment it would be expensive.
+	 */
+	virtual DataType dataType()=0;
+	
+	
+	/**
+	 * Subclasses can reimplement these methods to support writing some params into index file as configuration
+	 */
+	virtual void setParams(QString params) {}
+	virtual QString getParams() { return QString(); }
+	
+	/**
+	 * Extract features from image.
+	 * @param imageData Raw bitmap data in ARGB 32-bit format, as returned by QImage::bits(). Size of received data is width*height*4 bytes. Ordering is scan order (left-to-right, top-to-bottom), little-endian (BGRA).
+	 * @param width Number of pixels in image row.
+	 * @param height Number of rows in image.
+	 */
+	virtual FeatureVector extractFeatures(const uchar* imageData, int width, int height)=0;
+	
+	/**
+	 * Indexer uses this method to detect DCTFeatures class (see below).
+	 */
 	virtual bool isDct() { return false; }
 	
-	// Calculate distance between two feature vectors
+	/**
+	 * Calculate distance between two feature vectors.
+	 * This may be some specific algorithm for the actual features or it may be one of standard distance metrics.
+	 */
 	virtual double distance(FeatureVector f1, FeatureVector f2)=0;
 	
+	// Obsolete?
+	virtual void setAverage(FeatureVector f) {}
+	
 	bool debug;
+	
+	static ImageFeatures* factory(QString name);
+	
+	
+	// Some debugging functions
+	static void vectorDump(QVector<double> vector);
+	static void vectorDump(std::vector<double> vector);
+	static void vectorDump(std::vector<int> vector);
+	
+	// Variables of this feature that can be set from outside
+	// Feature constructor should define the list of variables and set them to their optimal values
+	struct Variable {
+		QString name;
+		double min, max, step, value;
+		Variable(QString name, double min, double max, double step, double value) : name(name), min(min), max(max), step(step), value(value) {}
+		Variable() : name(""), min(0), max(0), step(0) {}
+	};
+	QVector<Variable> variables;
+	double variableValues[10]; // This is faster :s
 };
 
-class DCTSearchAlgorithm : public SearchAlgorithm
+
+
+/**
+ * Base class for algorithms that work with DCT coefficients.
+ * DCTFeatures dont use method extractFeatures but has separate method processBlock that will be called once per each 8x8 block.
+ */
+
+class DCTFeatures : public ImageFeatures
 {
 public:
-	DCTSearchAlgorithm();
+	DCTFeatures();
 	
-	virtual QString name()=0; // should be a short string of lowercase letters and numerals (used in filename)
+	
+	/**
+	 * Method name (used as ID in index files, should be unique)
+	 */
+	virtual QString name()=0; 
+	
+	/**
+	 * Indexer uses this method to detect DCTFeatures class
+	 */
 	bool isDct() { return true; }
 	
-	// Call this before processing each image
+	/**
+	 * This method will be called once before processing each image
+	 */
 	virtual void init()=0;
-	// This will be called once for each 8x8 DCT block for given component
+	
+	/**
+	 * This method will be called once for each 8x8 DCT block.
+	 * Note that if chroma subsampling is used, this method will be called 4 times for component 0 then once for components 1 and 2 (last two 
+	 * represent a subsampled 16x16 block)
+	 * @param block Pointer to an array of 64 DCT coefficients in standard scanning order (not zig-zag order)
+	 * @param component ID of JPEG color component (0 = Y', 1 = Cb, 2 = Cr)
+	 */
 	virtual void processBlock(short int* block, int component)=0; 
-	// Call this after whole image is processed to get the final feature vector
+	
+	/**
+	 * This method will be called once after image processing is finished. 
+	 * Use this to calculate various statistics.
+	 */
 	virtual FeatureVector calculateVector()=0; 
 	
+	/**
+	 * Since libjpeg is C not C++ we must use this trick to call object method from libjpeg.
+	 */
 	static void processBlockWrapper(void* object, short int* block, int component) {
-		DCTSearchAlgorithm* myself = (DCTSearchAlgorithm*) object;
+		DCTFeatures* myself = (DCTFeatures*) object;
 		myself->processBlock(block, component);
 	}
 	
+	/**
+	 * Calculate distance between two feature vectors.
+	 * This may be some specific algorithm for the actual features or it may be one of standard distance metrics.
+	 */
 	virtual double distance(FeatureVector f1, FeatureVector f2)=0;
 	
 private:
 	// Disable this method
-	FeatureVector extractFeatures(const uchar*, int) { return FeatureVector(); }
+	FeatureVector extractFeatures(const uchar*, int, int) { return FeatureVector(); }
 };
 
-#endif // SEARCHALGORITHM_H
+#endif // IMAGEFEATURES_H
